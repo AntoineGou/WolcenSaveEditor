40c5cc548f46c0cd2aa4737d08bf9a25034a0139
Boobynette <antoine.goueslard@gmail.com>
2020-02-16 03:10:42 +0100

    Initial code Commit
    Solution Files
    Project,
    Directory Checker inside FileBackup
    TestProject
    Empty UI

---
+++ SaveEditorTest/FileBackupTests.cs
@@ -0,0 +1,45 @@
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using WolcenFileManagers;
+
+namespace SaveEditorTest
+{
+    public class FileBackupTests
+    {
+        private static readonly string BasePath = $"{Directory.GetCurrentDirectory()}/IntegrationTestsFiles";
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Category("unit")]
+        [Test]
+        public void FileBackup_Creation_ShouldBeInCreatedStatus()
+        {
+            var fileBackup = new FileBackup(BasePath);
+            Assert.AreEqual(FileBackupStatus.Created, fileBackup.Status);
+        }
+
+        [Category("unit")]
+        [Test]
+        public void FileBackup_LoadDirectory_OnRandomPath_ShouldReturnFalse()
+        {
+            var fileBackup = new FileBackup(BasePath + "NIMP");
+            Assert.AreEqual(false,fileBackup.LoadDirectory());
+            Assert.AreEqual(FileBackupStatus.Error, fileBackup.Status);
+        }
+
+        [Category("integration")]
+        [Test]
+        public void FileBackup_LoadDirectory_OnBaseFiles_ShouldDetect1Character()
+        {
+            var fileBackup = new FileBackup(BasePath);
+            Assert.AreEqual(true, fileBackup.LoadDirectory());
+            Assert.AreEqual(FileBackupStatus.Loaded,fileBackup.Status);
+            Assert.AreEqual(1, fileBackup.Characters.Count);
+            Assert.AreEqual("Bamboulorc", fileBackup.Characters.First());
+        }
+    }
+}
---
+++ WolcenFileManagers/FileBackup.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WolcenFileManagers
+{
+    public class FileBackup
+    {
+        private static readonly string PlayerChestFileName = "playerchest.json";
+        private static readonly string PlayerDataFileName = "playerdata.json";
+        private readonly DirectoryInfo _directory;
+        private static readonly string CharactersDirectoryName = "characters";
+
+
+        public FileBackup(string basePath)
+        {
+            _directory = new DirectoryInfo(basePath);
+        }
+
+        public FileBackupStatus Status { get; private set; } = FileBackupStatus.Created;
+        public List<string> Characters { get; set; } = new List<string>();
+
+        public bool LoadDirectory()
+        {
+            if (_directory.Exists)
+            {
+                var files = _directory.GetFiles("*.json");
+                if (files.Any(x => x.Name == PlayerChestFileName) && files.Any(x => x.Name == PlayerDataFileName))
+                {
+                    if (_directory.GetDirectories().Any(x => x.Name == CharactersDirectoryName))
+                    {
+                        var charFiles = _directory.EnumerateDirectories().Single(x => x.Name == CharactersDirectoryName)
+                            .GetFiles("*.json");
+                        foreach (var charFile in charFiles)
+                        {
+                            LoadCharacter(charFile);
+                        }
+                        Status = FileBackupStatus.Loaded;
+                        return true;
+                    }
+                }
+            }
+            Status = FileBackupStatus.Error;
+            return false;
+        }
+
+        private void LoadCharacter(FileInfo charFile)
+        {
+            if (charFile.Exists)
+            {
+                Characters.Add(charFile.Name.Replace(".json",""));
+            }
+        }
+    }
+
+    public enum FileBackupStatus
+    {
+        Created,
+        Loaded,
+        Error
+    }
+}
